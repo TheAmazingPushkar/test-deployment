6ef562f59caf2be5012ce87c03701a79c6500540
togoclovee <pushkarjr7@gmail.com>
2026-05-12 23:27:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -61,13 +61,12 @@ app = FastAPI()
 # IMPORTANT: Replace "*" with your Render frontend URL for security later
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://the-wild-lens.netlify.app/"],
+    allow_origins=["https://the-wild-lens.netlify.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# Load model (make sure cattle_model.h5 is in the same folder)
-model = tf.keras.models.load_model('cattle_model.h5')
+
 
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
